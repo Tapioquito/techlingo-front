10131efac2c57cd85fb40c37f5447b97760c6248
tapioquito <caioportoramos@gmail.com>
2024-03-06 13:52:15 -0300

    adicionada a fonte quicksand

--- techlingo-front/src/main.ts
+++ techlingo-front/src/main.ts
@@ -6,6 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'animate.css'
+import '@/assets/css/tailwind.css'
+import '@/assets/css/fonts.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
