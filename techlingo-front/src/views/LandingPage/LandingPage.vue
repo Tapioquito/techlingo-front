c8e98b0ffa8ad7e9efd2acc8cc148ae414b049e7
tapioquito <caioportoramos@gmail.com>
2024-03-07 17:02:37 -0300

    refatoradas as fontes e concluída a segunda seção

--- techlingo-front/src/views/LandingPage/LandingPage.vue
+++ techlingo-front/src/views/LandingPage/LandingPage.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="w-screen h-screen flex flex-col bg-slate-200">
+  <div class="w-full h-full flex flex-col bg-slate-200">
     <div class="w-full hero-height hero-background flex flex-col">
       <header class="w-full h-24 flex flex-row justify-between items-center">
-        <img src="./assets/images/techlingo_logo.png" class="w-32 h-10 ml-10" />
-        <button class="w-52 h-10 bg-white text-black rounded-full poppins mr-10">Começar</button>
+        <img src="../../assets/images/techlingo-logo.png" class="w-80 h-64 ml-20" />
+        <button class="w-52 h-10 bg-white text-black rounded-full poppins mr-10 hidden md:block">
+          Começar
+        </button>
       </header>
-      <div class="flex flex-row gap-x-12 justify-around">
+      <div class="flex flex-row gap-x-12 justify-around items-center">
         <div class="flex-flex-col w-96 h-96">
-          <div class="text-white hero-text-size quicksand font-bold">
+          <div class="text-white hero-text-size quicksand-bold font-bold text-4xl">
             <p>
               Leve sua <span class="text-red-500">carreira</span> de TI para o
               <span class="text-red-500"> próximo nível</span>
@@ -19,15 +21,15 @@
           </div>
           <div class="flex flex-row justify-evenly mt-5">
             <button class="text-white rounded-full bg-red-600 font-bold px-7 py-3">Começar</button>
-            <button class="text-white rounded-full border-slate-200 font-bold px-7 py-3">
+            <button class="text-white rounded-full bg-transparent white-border font-bold px-7 py-3">
               Saiba mais
             </button>
           </div>
         </div>
-        <div>
+        <div class="hidden md:block">
           <img
             src="../../assets/images/tech_bro_hero.png"
-            class="techbroimg rounded-tr-xl rounded-bl-xl"
+            class="techbroimg rounded-tr-xl rounded-bl-xl mb-20"
             alt=""
           />
         </div>
@@ -35,17 +37,17 @@
     </div>
 
     <div class="w-full hero-height flex flex-col items-center justify-center">
-      <div class="w-5/6 h-full">
+      <div class="w-5/6 h-full bg-green-700">
         <div class="flex flex-row items-center justify-center py-4">
           <h3 class="QuicksandBold font-medium text-black text-3xl">
             Chegou a hora de virar o jogo
           </h3>
         </div>
-        <div class="w-full h-full flex flex-row align-center justify-center gap-x-12">
+        <div class="w-full h-full flex flex-row align-center justify-center gap-x-6">
           <div
-            class="h-80 w-80 rounded-md bg-purple-700 flex flex-col align-center justify-center gap-y-8 text-center"
+            class="h-80 w-80 rounded-md bg-white flex flex-col items-center justify-evenly gap-x-8 text-center"
           >
-            <img src="../../assets/images/icon_bridge.png" alt="icone_ponte" />
+            <img src="../../assets/images/icon-bridge.png" alt="icone_ponte" class="w-28 h-14" />
             <h3 class="QuicksandBold font-medium text-black text-xl">Uma ponte para o sucesso</h3>
             <p class="QuicksandRegular text-slate-800">
               A good education website should have high-quality content that is accurate,
@@ -53,9 +55,9 @@
             </p>
           </div>
           <div
-            class="h-80 w-80 rounded-md bg-purple-700 flex flex-col align-center justify-center gap-y-8 text-center"
+            class="h-80 w-80 rounded-md bg-white flex flex-col items-center justify-evenly gap-x-8 text-center"
           >
-            <img src="../../assets/images/icon_bridge.png" alt="icone_ponte" />
+            <img src="../../assets/images/icon-key.png" alt="icone_chave" class="w-14 h-14" />
             <h3 class="QuicksandBold font-medium text-black text-xl">Uma chave que abre portas</h3>
             <p class="QuicksandRegular text-slate-800">
               A good education website should have high-quality content that is accurate,
@@ -63,9 +65,13 @@
             </p>
           </div>
           <div
-            class="h-80 w-80 rounded-md bg-purple-700 flex flex-col align-center justify-center gap-y-8 text-center"
+            class="h-80 w-80 rounded-md bg-white flex flex-col items-center justify-evenly gap-x-8 text-center"
           >
-            <img src="../../assets/images/icon_bridge.png" alt="icone_ponte" />
+            <img
+              src="../../assets/images/icon-door-open.png"
+              alt="icone_porta_aberta"
+              class="w-14 h-14"
+            />
             <h3 class="QuicksandBold font-medium text-black text-xl">Novas oportunidades</h3>
             <p class="QuicksandRegular text-slate-800">
               A good education website should have high-quality content that is accurate,
@@ -73,10 +79,15 @@
             </p>
           </div>
         </div>
+        <!-- <div class="flex w-full h-28">
+          <button class="text-white rounded-full bg-red-600 font-bold px-7 py-3">
+            Começar agora
+          </button>
+        </div> -->
       </div>
     </div>
-    <div class="w-full hero-height bg-yellow-500"></div>
-    <div class="w-full hero-height bg-purple-500"></div>
+    <div class="w-full hero-height bg-yellow-500">AAAAAAAAAAAAAAAAAAH</div>
+    <div class="w-full hero-height bg-purple-500">MIAAAAAAAAAAAAAUUUUUUU</div>
   </div>
 </template>
 <script>
@@ -85,6 +96,9 @@ export default {
 }
 </script>
 <style scoped>
+.white-border {
+  border-color: white;
+}
 .hero-background {
   background: linear-gradient(
     90deg,
@@ -99,5 +113,13 @@ export default {
 .techbroimg {
   width: 661px;
   height: 440px;
+}
+.icon-size {
+  height: 56px;
+  width: 56px;
+}
+.icon-bridge {
+  height: 56px;
+  width: 115px;
 }
 </style>
